c0ed47f7047177c6f9c6dabe399a02c458c89263
Sait_saiter <133907363+Madmateusz@users.noreply.github.com>
2025-04-28 21:38:22 +0200

    contactUs

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import './Header.css'
+
+function Header()
+{
+    return(
+        <header>
+            <ul>
+                <li><a href="">Główna</a></li>
+                <li><a href="">Oferta</a></li>
+                <li><a href="">Galeria</a></li>
+                <li><h1>Jacuzzi GO </h1></li>
+                <li><a href="">Opinie klientów</a></li>
+                <li><a href="">kontakt</a></li>
+                <li><a href="">FAQ</a></li>
+            </ul>
+
+        </header>
+    )
+}
+
+export default Header

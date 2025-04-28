19943dfa370e539677fc3eb017f0fb3478b954d4
Sait_saiter <133907363+Madmateusz@users.noreply.github.com>
2025-04-28 21:42:10 +0200

    dsa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,15 @@ function Header()
 {
     return(
         <header>
-            <ul>
-                <li><a href="">Główna</a></li>
+            {/* <ul> */}
+                {/* <li><a href="">Główna</a></li>
                 <li><a href="">Oferta</a></li>
-                <li><a href="">Galeria</a></li>
-                <li><h1>Jacuzzi GO </h1></li>
-                <li><a href="">Opinie klientów</a></li>
+                <li><a href="">Galeria</a></li> */}
+               <h1>Jacuzzi GO </h1>
+                {/* <li><a href="">Opinie klientów</a></li>
                 <li><a href="">kontakt</a></li>
-                <li><a href="">FAQ</a></li>
-            </ul>
+                <li><a href="">FAQ</a></li> */}
+            {/* </ul> */}
 
         </header>
     )
